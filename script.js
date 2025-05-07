43db38223dc452b2ba150f67b6a94e68f83003bb
Arif <azhar1jnp@gmail.com>
2025-05-07 17:02:42 +0530

    to-do list project

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box")
+const ListContainer = document.getElementById("List-container")
+function AddTask(){
+    if(inputBox.value === '')
+    {
+        alert("You Must Write Something")
+    }
+    else{
+        let li =document.createElement("li");
+        li.innerHTML = inputBox.value;
+        ListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value ="";
+    saveData()
+}
+
+ListContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false)
+function saveData() 
+{
+    localStorage.setItem("data",ListContainer.innerHTML);
+    
+}
+function showTask(){
+    ListContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+// alert("Hello from JavaScript!");
+// document.body.innerHTML = "<h1 style='color: green;'>Welcome to JavaScript!</h1>";
